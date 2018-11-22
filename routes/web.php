8352673a08130185ac2f5b4170975d1628c42056
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-11-22 12:02:23 +0100

    Sync

--- routes/web.php
+++ routes/web.php
@@ -14,50 +14,8 @@ Route::get('/', function () {
 Route::get('docs', 'DocsController@showRootPage');
 Route::get('docs/{version}/{page?}', 'DocsController@show');
 
-Route::get('partners', function () {
-    return view('partners');
-});
-
-Route::get('/partner/tighten', function () {
-    return view('community-partner-tighten');
-});
-
-Route::get('/partner/vehikl', function () {
-    return view('community-partner-vehikl');
-});
-
-Route::get('/partner/kirschbaum-development-group', function () {
-    return view('community-partner-kirschbaum');
-});
-
-Route::get('/partner/byte5', function () {
-    return view('community-partner-byte5');
-});
-
-Route::get('/partner/64robots', function () {
-    return view('community-partner-64robots');
-});
-
-Route::get('/partner/cubet', function () {
-    return view('community-partner-cubet');
-});
-
-Route::get('/partner/dev-squad', function () {
-    return view('community-partner-dev-squad');
-});
-
-Route::get('/partner/ideil', function () {
-    return view('community-partner-ideil');
-});
-
 Route::get('/discord', function () {
-    return redirect('https://discord.gg/wTgA8jR');
+    dump('TODO');
+//    return redirect('https://discord.gg/wTgA8jR');
 });
 
-Route::get('/certification', function () {
-    return redirect('https://certification.laravel.com');
-});
-
-// Route::get('/partner/insider', function () {
-//     return view('community-partner-insider');
-// });
